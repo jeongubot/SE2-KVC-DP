a306154ac0f146e5a8ead4c4ef52c16470899c40
micahmallari <micahjbm19@gmail.com>
2025-05-15 21:36:38 +0800

    modify navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link as RouterLink, useLocation } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
-import "../css/NavBar.css";
+import "../css/Navbar.css";
 import { Button } from "./Button";
 
 function NavBar() {
